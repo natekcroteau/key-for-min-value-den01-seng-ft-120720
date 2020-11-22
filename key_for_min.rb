4d5cd2873e80a0f1761d20ad25ae1c47194a6c3e
Nate Croteau <natekcroteau@gmail.com>
2020-11-22 22:56:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,11 @@ low_val = 999999999999
 low_key = ""
  
 name_hash.each do |key, value|
+   
    if value < low_val 
      low_val = value 
      low_key = key
-   else name_hash.empty? == nil  
+   
     end
   end 
 low_key
